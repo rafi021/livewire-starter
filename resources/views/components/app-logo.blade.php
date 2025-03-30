f295a4a9906866c38e1a1391e6fba08d374a766d
rafi021 <mahmud.ibrahim021@gmail.com>
2025-03-30 21:42:47 +0600

    Header and Color

--- resources/views/components/app-logo.blade.php
+++ resources/views/components/app-logo.blade.php
@@ -1,6 +1,7 @@
-<div class="flex aspect-square size-8 items-center justify-center rounded-md bg-accent-content text-accent-foreground">
-    <x-app-logo-icon class="size-5 fill-current text-white dark:text-black" />
+<div
+    class="bg-linear-to-r from-blue-500 to-blue-600  text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-md bg-accent-content text-accent-foreground">
+    <x-app-logo-icon class="size-5 fill-current text-blue-100 dark:text-black" />
 </div>
 <div class="ml-1 grid flex-1 text-left text-sm">
-    <span class="mb-0.5 truncate leading-none font-semibold">Laravel Starter Kit</span>
+    <span class="mb-0.5 truncate leading-none font-semibold">Livewire Tasks</span>
 </div>
